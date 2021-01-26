4b0c3a6c931fd79175e25744a8a34dcd4742c4d9
Jacob Klimenko <droidedjake@gmail.com>
2021-01-26 15:13:10 -0500

    commit tree 2f8ef18b68937be8d10a91a1bad28227d23f5852

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
+
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(stats)
+        stats.each do |key, value|
+            self.send(("#{key}="), value)
+        end
+        save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
+
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,27 @@
+
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(stats)
+        stats.each do |key, value|
+            self.send(("#{key}="), value)
+        end
+
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
 end
+
